493272542d2482c108167c2ae9bf739a77e3ea7a
Themiya <themiyawijenayaka100@gmail.com>
2026-04-17 19:40:09 +0530

    Added navigation bar and base template layout

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "jobportal.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
