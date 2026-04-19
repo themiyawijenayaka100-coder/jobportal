121490f30855ba8333c8784fc9844ff8f4660c88
kmethsara <jdkmethsara@gmail.com>
2026-04-19 13:11:06 +0530

    Implement application management view and routing to urls

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('create/', views.create_job, name='create_job'),
     path('dashboard/', views.employer_dashboard, name='employer_dashboard'),
+    path('<int:job_id>/applications/', views.manage_job_applications, name='manage_job_applications'),
     path('', views.job_list, name='job_list'),
 ]
